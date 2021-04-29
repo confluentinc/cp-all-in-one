52002a23e8a2e3bfff1ef84878df8947bf14be23
Yeva Byzek <yeva@confluent.io>
2021-04-29 17:17:46 -0400

    Tweaks

--- cp-all-in-one-community/update_run.sh
+++ cp-all-in-one-community/update_run.sh
@@ -5,7 +5,4 @@ sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
 sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
 echo "kafka-storage format -t $(kafka-storage random-uuid) -c /etc/kafka/kafka.properties" >> /etc/confluent/docker/ensure
 
-echo "mkdir /tmp/kraft-combined-logs"
-mkdir /tmp/kraft-combined-logs
-
 sed -i '/docker\/configure/a \\ncat /tmp/myprops.properties >> /etc/kafka/kafka.properties' /etc/confluent/docker/run
