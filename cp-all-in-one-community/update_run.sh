9c4100f6fa559ce360e9657c82062fad0e1847d0
Yeva Byzek <yeva@confluent.io>
2021-04-30 08:22:15 -0400

    Update comments in script

--- cp-all-in-one-community/update_run.sh
+++ cp-all-in-one-community/update_run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
+# Docker workaround: Remove check for KAFKA_ZOOKEEPER_CONNECT parameter
 sed -i '/KAFKA_ZOOKEEPER_CONNECT/d' /etc/confluent/docker/configure
 
-# Ignore cub zk-ready
+# Docker workaround: Ignore cub zk-ready
 sed -i 's/cub zk-ready/echo ignore zk-ready/' /etc/confluent/docker/ensure
 
-# Format the storage directory with a new cluster ID
+# KRaft required step: Format the storage directory with a new cluster ID
 echo "kafka-storage format -t $(kafka-storage random-uuid) -c /etc/kafka/kafka.properties" >> /etc/confluent/docker/ensure
