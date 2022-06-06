2d63c5d1dd8435d6955574c72e2658760e82e1d0
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-06-05 22:47:28 -0400

    Revert "Add required fields for new GitHub Action (#95)" (#96)
    This reverts commit 54398817e755bb49fff0ca1edd027f5aa1bcb726.

--- entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-wget -O docker-compose.yml https://raw.githubusercontent.com/confluentinc/cp-all-in-one/$INPUT_GITHUB_BRANCH_VERSION/cp-all-in-one/docker-compose.yml
-docker-compose up -d $INPUT_SERVICE
-sleep 20
