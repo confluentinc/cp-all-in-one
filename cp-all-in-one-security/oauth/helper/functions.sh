ca842ca68cc7ff7c6089514cb902c9a85c3d36d8
Ujjwal <ukalia@confluent.io>
2024-05-14 15:33:56 +0530

    [ga-examples] | fixing connect for prefix

--- cp-all-in-one-security/oauth/helper/functions.sh
+++ cp-all-in-one-security/oauth/helper/functions.sh
@@ -2,7 +2,7 @@
 
 assign_role_bindings()
 {
-    
+
 docker exec broker curl http://broker:8091 -H "Authorization: Bearer $auth_token" -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://broker:8091/security/1.0/principals/Group:/app_group1/roles/SecurityAdmin -d '{"clusters":{"kafka-cluster":"vHCgQyIrRHG8Jv27qI2h3Q", "schema-registry-cluster":"schema-registry"}}'
 
 docker exec broker curl http://broker:8091 -H "Authorization: Bearer $auth_token" -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://broker:8091/security/1.0/principals/Group:/app_group1/roles/ResourceOwner/bindings -d '{"scope":{"clusters":{"kafka-cluster":"vHCgQyIrRHG8Jv27qI2h3Q"}}, "resourcePatterns":[{"resourceType":"Group", "name":"schema-registry-demo", "patternType":"LITERAL"}]}'
@@ -32,6 +32,7 @@ docker exec broker curl http://broker:8091 -H "Authorization: Bearer $auth_token
 docker exec broker curl http://broker:8091 -H "Authorization: Bearer $auth_token" -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://broker:8091/security/1.0/principals/User:5efae8dc-08e8-4980-8453-cb81375a029b/roles/SystemAdmin -d '{"clusters":{"kafka-cluster":"vHCgQyIrRHG8Jv27qI2h3Q", "schema-registry-cluster":"schema-registry"}}'
 
 docker exec broker curl http://broker:8091 -H "Authorization: Bearer $auth_token" -i -H "Content-Type: application/json" -H "Accept: application/json" -X POST http://broker:8091/security/1.0/principals/User:5efae8dc-08e8-4980-8453-cb81375a029b/roles/SystemAdmin -d '{"clusters":{"kafka-cluster":"vHCgQyIrRHG8Jv27qI2h3Q","connect-cluster":"connect-cluster"}}'
+
 }
 
 build_connect_image()
@@ -44,7 +45,7 @@ build_connect_image()
   DOCKERFILE="${DIR}/../Dockerfile"
   CONTEXT="${DIR}/."
   echo "docker build -t localbuild/connect:${CONFLUENT_DOCKER_TAG}-${CONNECTOR_VERSION} -f $DOCKERFILE $CONTEXT"
-  docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=confluentinc -t localbuild/connect:master-latest -f $DOCKERFILE $CONTEXT || {
+  docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${DOCKER_REGISTRY} -t localbuild/connect:master-latest -f $DOCKERFILE $CONTEXT || {
     echo "ERROR: Docker image build failed. Please troubleshoot and try again. For troubleshooting instructions see https://docs.confluent.io/platform/current/tutorials/cp-demo/docs/troubleshooting.html"
     exit 1
   }
